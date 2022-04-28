2407fac56ac0b3932726fa87d52145413f6bfc74
ester kim <kimester1996@gmail.com>
2022-04-27 23:40:02 -0800

    fixed other error now trying to link front and back

--- Develop/ester.js
+++ Develop/ester.js
@@ -7,7 +7,7 @@ const taker = require('./db/db.json');
 //routes
 
 app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./index.html"))
+    res.sendFile(path.join(__dirname,"public/index.html"))
 })
 
 app.get("/db/db",(req,res)=>{
