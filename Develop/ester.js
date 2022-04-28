4b8a6c5404be570e6cabd8b36a34d84e8ebfd580
ester kim <kimester1996@gmail.com>
2022-04-28 00:13:42 -0800

    got passed the initial button

--- Develop/ester.js
+++ Develop/ester.js
@@ -4,7 +4,15 @@ const app = express();
 const PORT = 3000;
 const taker = require('./db/db.json');
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 //routes
+
+app.get("/notes",(req,res)=>{
+    res.sendFile(path.join(__dirname,"public/notes.html"))
+})
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"public/index.html"))
@@ -17,3 +25,4 @@ app.get("/db/db",(req,res)=>{
 app.listen(PORT,()=>{
     console.log("listinging to port"+ PORT)
 })
+
--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
-console.log("linked")
-// const notes = document.querySelector()
-fetch("/db/db").then(res=>res.json()).then(data=>{
-    console.log(data)
+// console.log("linked")
+// // const notes = document.querySelector()
+// fetch("/db/db").then(res=>res.json()).then(data=>{
+//     console.log(data)
 
-})
+// })
